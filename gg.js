d48e7815762bfe273accec3ddbb0f821be4c5cfb
azadabid <58371305+azadabid@users.noreply.github.com>
2019-11-30 21:48:24 +0300

    Add files via upload

---
+++ gg.js
@@ -0,0 +1,38 @@
+function sft2()
+{
+   x=a[n-1] 
+    for (i=n-1; i>=1; i--)
+   {
+       a[i]=a[i-1]
+   }
+   a[0]=x;     //the first index is changes by the last element w/c is x=a[n-1]
+   for (i=0; i<a.length;i++)
+   return a
+   }
+let a=[1,3,4,5,7,9,8,9,87]
+console.log(a)
+let x,i;
+let n=a.length           //set the limit of ittration by n
+let j=0
+               // the starting element is the set to x
+
+console.log(sft2())
+console.log(`\n`)
+let as = require('readline-sync');
+let k=as.questionFloat("Enter number of shift"+" ");     //here set the number of shift 
+while(j<k){   //loop the function k-1 times and console the shifted array 
+   
+}
+ 
+    x=a[n-1];
+    for (i=n-1; i>=1; i--)
+   {
+    a[i]=a[i-1]
+    }
+    a[0]=x;
+    for (i=0; i<a.length;i++) 
+ return a
+ }    console.log(sft())
+      j++
+ }
+
